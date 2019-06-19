b30bb4f2392bb426c61ce48773bd44262636cf35
angga88888 <fingerstyle88888@gmail.com>
2019-06-19 20:18:19 +0800

    aa

--- src/main_code.py
+++ src/main_code.py
@@ -5,51 +5,96 @@ import math
 from scipy.io import wavfile
 from sklearn.metrics import mean_squared_error
 from sklearn.decomposition import FastICA
-from get_sources.data_utils import Audio
+from sklearn.decomposition import SparsePCA
 
 
-n_loop = 1
-n_sources = 2
-Audio(nb_tracks=n_sources, param=n_loop).load_tracks(param=n_loop)
+def ica(X):
+	# Compute ICA
+	ica = FastICA()
+	S_ = ica.fit(X).transform(X)  # Get the estimated sources
+	A_ = ica.mixing_  # Get estimated mixing matrix
+	np.allclose(X, np.dot(S_, A_.T) + ica.mean_)
+	return S_
+
+
+def sca(X):
+	# Compute ICA
+	sca = SparsePCA()
+	S_ = sca.fit(X).transform(X)  # Get the estimated sources
+	# ica = FastICA()
+	A_ = ica.mixing_
+	# print(np.allclose(X, S_))
+	print(X)
+	print(S_)
+	print(np.allclose(X, np.dot(S_, A_.T) + np.mean(sca.components_ == 0)))
+	return S_
+
+
+def mse(S_):
+	#pengujian metode MSE
+	MSE = mean_squared_error(S_[:,0]/1000000, S_[:,1]/1000000)
+	return MSE
+
+
+def sir(MSE):
+	#pengujian metode SIR
+	SIR = -10 * math.log10(MSE)
+	return SIR
+
+
+def write_wav(X, fs_2, S_, MSE, SIR, method, estimated=True, write_matrix=True, write_wav=True):
+
+	if method == "ica":
+		#if the using method is ICA
+		multiply_factor = 1000000 ;
+
+		temp_output_1 = multiply_factor*S_[:,0]
+		temp_output_2 = multiply_factor*S_[:,1]	
+
+	if method == "sca":
+		temp_output_1 = S_[:,0]
+		temp_output_2 = S_[:,1]	
+
+	if write_matrix == True:
+		#write matriks of data
+		print("Matriks pemisahan suara 1 metode "+method+": " + str(S_[:,0]))
+		print("Matriks pemisahan suara 2 metode "+method+": " + str(S_[:,1]))
+
+	if estimated == True:
+		print("Estimasi error MSE metode "+method+": " + str(MSE))
+		print("Estimasi error SIR metode "+method+": " + str(SIR))
+
+	if write_wav == True:
+		#write mixing wav audio
+		wavfile.write("../results/mix_audio/1_mix_"+method+".wav", fs_2, X.astype(np.int16))
+
+		#write separates wav audio
+		wavfile.write("../results/separates_audio/1_vokal_separate_"+method+".wav", fs_2, temp_output_2.astype(np.int16))
+		wavfile.write("../results/separates_audio/1_instrumen_separate_"+method+".wav", fs_2, temp_output_1.astype(np.int16))
+
+
+method = "sca"
 
 fs_1, voice_1 = wavfile.read("../results/sources_audio/1_vokal_source.wav")
 fs_2, voice_2 = wavfile.read("../results/sources_audio/1_instrumen_source.wav")
 m, = voice_1.shape
 voice_2 = voice_2[:m]
 
-
 S = np.c_[voice_1, voice_2]
 # A = np.array([[1, 1], [0.5, 2]])
 A = np.array([[0.3816, 0.8678], [0.8534, -0.5853]])  # Mixing matrix
 X = np.dot(S, A.T)  # Generate observations
 
-# Compute ICA
-ica = FastICA()
-S_ = ica.fit(X).transform(X)  # Get the estimated sources
-A_ = ica.mixing_  # Get estimated mixing matrix
-np.allclose(X, np.dot(S_, A_.T))
-
-multiply_factor = 1000000 ;
-
-temp_output_1 = multiply_factor*S_[:,0]
-temp_output_2 = multiply_factor*S_[:,1]
-
-#write mixing wav audio
-wavfile.write("../results/mix_audio/1_mix" + ".wav", fs_2, X.astype(np.int16))
-
-print(temp_output_1)
-print(temp_output_2)
-
-MSE = mean_squared_error(temp_output_1, temp_output_2)
-print(MSE)
-
-SIR = -10 * math.log10(MSE)
-print(SIR)
-
-#write separates wav audio
-wavfile.write("../results/separates_audio/1_vokal_separate" + ".wav", fs_2, temp_output_1.astype(np.int16))
-wavfile.write("../results/separates_audio/1_instrumen_separate" + ".wav", fs_2, temp_output_2.astype(np.int16))
-
+if method == "ica":
+	S_ = ica(X)
+	MSE_ica = mse(S_)
+	SIR_ica = sir(MSE_ica)
+	write_wav(X,fs_2,S_,MSE_ica,SIR_ica,method)
+else:
+	S_ = sca(X)
+	MSE_sca = mse(S_)
+	SIR_sca = sir(MSE_sca)
+	write_wav(X,fs_2,S_,MSE_sca,SIR_sca,method)
 
 # Plot results
 # pl.figure()
